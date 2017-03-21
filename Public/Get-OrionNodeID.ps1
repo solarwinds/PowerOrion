466bd8156e83f6e191e05c33db71715f7758c6e9
Michael Halpin <michael.halpin@gmail.com>
2017-03-21 22:41:09 +0000

    First function added, Get-OrionNodeID. Also added pester tests. Removed -all switch, so all calls return same output, an individual int

---
+++ Public/Get-OrionNodeID.ps1
@@ -0,0 +1,73 @@
+﻿<#
+    .Synopsis
+    Returns the Node ID for a given node managed in Orion
+    .DESCRIPTION
+    This CmdLet returns the Node ID for a given node managed in Orion, by looking up either the node name or IP Address. 
+   
+    .EXAMPLE
+    Get-OrionNodeID -node "lab-hpinsight" -swisconnection $swis
+    .EXAMPLE
+    Get-OrionNodeID -IPAddress 10.199.1.100 -SwisConnection $swis
+    .EXAMPLE 
+    $swis = Connect-Swis -UserName admin -Password "" -Hostname 10.160.5.75
+    Get-OrionNodeID -Node $TestNode -SwisConnection $swis
+    
+    3
+                                                                                                        
+ 
+#>
+function Get-OrionNodeID
+{
+  [CmdletBinding()]
+  [OutputType([int])]
+  Param
+  (
+   
+    #The Caption or Nodename used to reference the entity
+    [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=0,
+    Parametersetname="Name")]
+    [validatenotnullorempty()]
+    [alias("Node","Caption")]
+    [string]
+    $NodeName,
+
+    [Parameter(Mandatory=$true,
+        ValueFromPipelineByPropertyName=$true,
+        Position=0,
+    Parametersetname="IP")]
+    [validatenotnullorempty()]
+    [alias("IP")]
+    [String]$IPAddress,
+        
+    #SolarWinds Information Service (SWIS) Connection
+    [parameter(mandatory=$true)]
+    [validatenotnullorempty()]
+    [SolarWinds.InformationService.Contract2.InfoServiceProxy]
+    $SwisConnection
+  )
+
+  Begin
+  {
+    Write-Verbose "Starting $($myinvocation.mycommand)"  
+  }
+  Process
+  {   
+    
+      if ($NodeName){
+        write-verbose " Querying Orion Server for Node ID for $NodeName"
+        $NodeID = Get-SwisData $SwisConnection "SELECT NodeID FROM Orion.Nodes WHERE Caption=@n" @{n=$NodeName}
+      } else
+      {
+        write-verbose " Querying Orion Server for Node ID for $IPAddress"
+        $NodeID = Get-SwisData $SwisConnection "SELECT NodeID FROM Orion.Nodes WHERE IP_Address=@ip" @{ip=$IPAddress}
+      }
+   
+  }
+  End
+  {
+    Write-Verbose "Finishing $($myinvocation.mycommand)"
+    Return $NodeID
+  }
+}
---
+++ Tests/Get-OrionNodeID.tests.ps1
@@ -0,0 +1,56 @@
+﻿# this is a Pester test file
+
+#. ($PSCommandPath -replace '\.tests\.ps1$', '.ps1')
+$PSCommandPath
+$CodeFile = $PSCommandPath
+. (($CodeFile -replace '\\tests\\', '\public\')  -replace '\.tests\.ps1$', '.ps1')
+
+# initialize SWIS connection 
+if (Get-PSSnapin -Name SwisSnapin -ErrorAction SilentlyContinue){
+  remove-PSSnapin SwisSnapin
+}
+Add-PSSnapin SwisSnapin 
+$MyInvocation.PSScriptRoot
+$settings = Get-Content "$PSScriptRoot\PesterConfig.json" |  ConvertFrom-Json 
+$user = $settings.User
+$password=$settings.Password
+$OrionServer = $settings.OrionServer
+$swis = Connect-Swis -UserName $user -Password $password -Hostname $OrionServer
+
+
+#endregion
+
+#describes the function Get-OrionNodeID
+Describe 'Get-OrionNodeID' {
+
+  # scenario 1: call the function without arguments
+  Context 'Running with -IPAddress'   {
+    # test 1: it does not throw an exception:
+    It 'runs without errors' {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-OrionNodeID -IPAddress '192.168.100.2' -SwisConnection $swis} | Should Not Throw
+    }
+    It 'returns an  int ' {
+      (Get-OrionNodeID -IPAddress '192.168.100.2' -SwisConnection $swis).gettype() | should be int
+    }
+    
+   
+  }
+  
+  # scenario 1: call the function without arguments
+  Context 'Running with -NodeName'   {
+    # test 1: it does not throw an exception:
+    It 'runs without errors' {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-OrionNodeID -NodeName 'OrionVM' -SwisConnection $swis} | Should Not Throw
+    }
+    It 'returns an object' {
+        (Get-OrionNodeID -NodeName 'OrionVM' -SwisConnection $swis).GetType() | should be int
+    }
+  }
+  
+}
