9f1ad1fdac47d72fd553b9e33dcc6b5ba75d1741
Michael Halpin <michael.halpin@gmail.com>
2017-03-21 23:21:42 +0000

    Converted Get-OrionNode & tests

---
+++ Public/Get-OrionNode.ps1
@@ -0,0 +1,93 @@
+﻿<#
+    .Synopsis
+    Returns the properties, or the custom properties, of a node monitored by Orion
+    .DESCRIPTION
+    This Cmdlet returns the properties of a node monitored by Orion. It can look up a node based on it's node id. If this is not explicitly available, it can call Get-OrionNodeID
+    If passed the  -custom switch it can return
+
+    .EXAMPLE
+    Get-OrionNodeProperties -SwisConnection $swis -NodeID $nodeid   
+
+    .EXAMPLE
+     Get-OrionNodeProperties -SwisConnection $swis  -NodeID $nodeid  -custom
+
+    Key                                Value
+    ---                                -----
+    NodeID                             3
+    City                               Austin
+    IsMissionCritical                  False
+
+#>
+function Get-OrionNode
+{
+  [CmdletBinding()]
+  [OutputType([psobject])]
+  Param
+  (
+    #SolarWinds Information Service (SWIS) Connection
+    [parameter(mandatory=$true)]
+    [validatenotnullorempty()]
+    [SolarWinds.InformationService.Contract2.InfoServiceProxy]
+    $SwisConnection,
+        
+    [Parameter(ValueFromPipelineByPropertyName=$true,
+    Parametersetname="ID")]
+        
+    [validatenotnullorempty()]
+    [alias("ID")]
+    [int32]
+    $NodeID,
+      
+    #The IP Address of the node
+    [Parameter(ValueFromPipelineByPropertyName=$true,
+    Parametersetname="IP")]
+    [String]$IPAddress,
+
+
+
+
+    #Returns custom properties if set
+    [switch]
+    [alias('custom')]
+    $customproperties
+  )
+
+  Begin
+  {
+    Write-Verbose "Starting $($myinvocation.mycommand)"  
+    $OrionServer = Get-OrionHostFromSwisConnection -swisconnection $SwisConnection
+    write-debug " The value of OrionServer is $OrionServer"
+
+    if($IPAddress){
+      write-debug " The value of IPAddress is $IPAddress"
+      write-verbose " IP passed, calling Get-OrionNodeID for $IPAddress"
+      $ID = Get-OrionNodeID -IPAddress $IPAddress -SwisConnection $SwisConnection
+    }else {
+            
+      write-debug " The value of ID is $NodeID"
+      write-verbose " Integer passed"
+      $ID = $NodeID           
+    }
+           
+        
+    if ($customproperties){
+      $uri = "swis://$OrionServer/Orion/Orion.Nodes/NodeID=$ID/CustomProperties"
+    } else {
+      $uri = "swis://$OrionServer/Orion/Orion.Nodes/NodeID=$ID"
+    }
+    Write-Debug " The URI is $uri"
+  }
+  Process
+  {
+    Write-Verbose " Getting properties at $uri"
+    $nodeProps = Get-SwisObject $SwisConnection -Uri $uri
+    $properties = New-Object -TypeName psobject -Property $nodeProps
+    write-debug " The value of nodeprops is $nodeProps"
+    write-debug " The value of properties is $($properties.gettype())"
+  }
+  End
+  {        
+    Write-Verbose "Finishing $($myinvocation.mycommand)"
+    Write-Output $properties
+  }
+}
---
+++ Tests/Get-OrionNode.tests.ps1
@@ -0,0 +1,96 @@
+﻿# this is a Pester test file
+
+#. ($PSCommandPath -replace '\.tests\.ps1$', '.ps1')
+$PSCommandPath
+$CodeFile = $PSCommandPath
+. (($CodeFile -replace '\\tests\\', '\public\')  -replace '\.tests\.ps1$', '.ps1')
+
+# initialize SWIS connection 
+if (Get-PSSnapin -Name SwisSnapin -ErrorAction SilentlyContinue){
+  remove-PSSnapin SwisSnapin
+}
+Add-PSSnapin SwisSnapin 
+$MyInvocation.PSScriptRoot
+$settings = Get-Content "$PSScriptRoot\PesterConfig.json" |  ConvertFrom-Json 
+$user = $settings.User
+$password=$settings.Password
+$OrionServer = $settings.OrionServer
+$swis = Connect-Swis -UserName $user -Password $password -Hostname $OrionServer
+
+
+#endregion
+
+#describes the function Get-OrionNodeID
+Describe 'Get-OrionNode' {
+
+  # scenario 1: call the function without arguments
+  Context 'Running with -IPAddress'   {
+    # test 1: it does not throw an exception:
+    It 'runs without errors' {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-OrionNode -IPAddress '192.168.100.2' -SwisConnection $swis} | Should Not Throw
+    }
+    It 'returns an  object ' {
+      (Get-OrionNode -IPAddress '192.168.100.2' -SwisConnection $swis).gettype() | should be System.Management.Automation.PSCustomObject
+    }
+    It 'returns a URI member ' {
+      (Get-OrionNode -IPAddress '192.168.100.2' -SwisConnection $swis).uri | should be 'swis://OrionVM./Orion/Orion.Nodes/NodeID=1'
+    }
+   
+  }
+  
+  Context 'Running with -NodeID'   {
+    # test 1: it does not throw an exception:
+    It 'runs without errors' {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-OrionNode -NodeID 1 -SwisConnection $swis} | Should Not Throw
+    }
+    It 'returns an  object ' {
+      (Get-OrionNode -NodeID 1 -SwisConnection $swis).gettype() | should be System.Management.Automation.PSCustomObject
+    }
+    It 'returns a URI member ' {
+      (Get-OrionNode -NodeID 1 -SwisConnection $swis).uri | should be 'swis://OrionVM./Orion/Orion.Nodes/NodeID=1'
+    }
+   
+  }
+  
+  # scenario 1: call the function without arguments
+  Context 'Running with -IPAddress -customproperties'   {
+    # test 1: it does not throw an exception:
+    It 'runs without errors' {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-OrionNode -IPAddress '192.168.100.2' -customproperties -SwisConnection $swis} | Should Not Throw
+    }
+    It 'returns an  object ' {
+      (Get-OrionNode -IPAddress '192.168.100.2' -customproperties -SwisConnection $swis).gettype() | should be System.Management.Automation.PSCustomObject
+    }
+    It 'returns a URI member ' {
+      (Get-OrionNode -IPAddress '192.168.100.2' -customproperties -SwisConnection $swis).uri | should be 'swis://OrionVM./Orion/Orion.Nodes/NodeID=1/CustomProperties'
+    }
+   
+  }
+  
+  Context 'Running with -NodeID -customproperties'   {
+    # test 1: it does not throw an exception:
+    It 'runs without errors' {
+      # Gotcha: to use the "Should Not Throw" assertion,
+      # make sure you place the command in a 
+      # scriptblock (braces):
+      { Get-OrionNode -NodeID 1 -customproperties -SwisConnection $swis} | Should Not Throw
+    }
+    It 'returns an  object ' {
+      (Get-OrionNode -NodeID 1 -customproperties -SwisConnection $swis).gettype() | should be System.Management.Automation.PSCustomObject
+    }
+    It 'returns a URI member ' {
+      (Get-OrionNode -NodeID 1 -customproperties -SwisConnection $swis).uri | should be 'swis://OrionVM./Orion/Orion.Nodes/NodeID=1/CustomProperties'
+    }
+   
+  }
+  
+}
