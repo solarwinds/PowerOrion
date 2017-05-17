5b7f11a32bf988131d331b77c4831653660afb5e
Michael Halpin <michael.halpin@gmail.com>
2017-05-17 14:57:03 +0100

    Moved connection code for tests to single file. Add-swissnappin has to be called in each file however

---
+++ Tests/Test-PowerOrion.ps1
@@ -0,0 +1,32 @@
+﻿Set-Location C:\projects\PowerOrion\Tests
+
+if (Get-PSSnapin -Name SwisSnapin -ErrorAction SilentlyContinue){
+  remove-PSSnapin SwisSnapin
+}
+#silence issues relating to snapins being called from modules
+try
+{
+  add-PSSnapin SwisSnapin  -ErrorVariable $SwisError
+ }
+ catch{}
+
+
+$settings = Get-Content "$PSScriptRoot\PesterConfig.json" |  ConvertFrom-Json 
+$user = $settings.User
+$password=$settings.Password
+$OrionServer = $settings.OrionServer
+$ModPath = $settings.ModulePath
+
+Import-Module -name $ModPath -Force 
+$global:swis = Connect-Swis -UserName $user -Password $password -Hostname $OrionServer
+
+Invoke-Pester 
+
+<#foreach ($file in Get-ChildItem -Path ..\Public\*.ps1){
+    Invoke-Pester -CodeCoverage $file 
+    }
+
+#>
+if (Get-PSSnapin -Name SwisSnapin -ErrorAction SilentlyContinue){
+  remove-PSSnapin SwisSnapin
+}
