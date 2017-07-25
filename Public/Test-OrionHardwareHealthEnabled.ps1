c9e6a0ddff0e3a9ad1e3be3334b5689b0e5742c4
Michael Halpin <michael.halpin@gmail.com>
2017-07-25 12:45:26 +0100

    Added test-OrionHardwareHealthEnabled

---
+++ Public/Test-OrionHardwareHealthEnabled.ps1
@@ -0,0 +1,61 @@
+﻿<#
+.Synopsis
+   Test if Orion node has a hardware health monitoring enabled
+.DESCRIPTION
+   Takes an array of node IDs, and checks using the IsHardwareHealthEnabled  verb, if hardware health is enabled
+.EXAMPLE
+  Test-OrionHardwareHealthEnabled -SwisConnection $swis -NodeID 3,5 
+  HardwareEnabled node
+--------------- ----
+           True 3   
+          False 5 
+
+#>
+function Test-OrionHardwareHealthEnabled
+{
+    [CmdletBinding()]
+    [Alias()]
+    
+    Param
+    (
+    #SolarWinds Information Service (SWIS) Connection
+    [parameter(mandatory=$true)]
+    [validatenotnullorempty()]
+    [SolarWinds.InformationService.Contract2.InfoServiceProxy]
+    $SwisConnection,
+        
+    [parameter(mandatory=$true)]   
+    [validatenotnullorempty()]
+    [int32[]]
+    $NodeID
+    )
+
+    Begin
+    {
+      $results = @()
+    }
+    Process
+    {
+        foreach ($node in $NodeID){
+          write-verbose "Checking Node ID:$node for hardware health"
+          $RawResult = Invoke-SwisVerb -EntityName Orion.HardwareHealth.HardwareInfo -Verb IsHardwareHealthEnabled -arguments @("N:$Node") -SwisConnection $SwisConnection
+          $filtered = $RawResult | Select-Object '#text'
+          write-debug "The result is $filtered"
+          if ($filtered.'#text'){
+            $check = @{'node'="$Node"
+                          'HardwareEnabled'=$true}
+          }
+          else{
+              $check = @{'node'="$Node"
+                            'HardwareEnabled'=$false}
+          }
+          $results += New-Object PSobject -Property $check
+        }
+    }
+    End
+    {
+      Write-Output $results
+    }
+}
+
+
---
+++ Tests/Test-OrionHardwareHealthEnabled.test.ps1
@@ -0,0 +1,35 @@
+﻿# this is a Pester test file
+
+#. ($PSCommandPath -replace '\.tests\.ps1$', '.ps1')
+$PSCommandPath
+$CodeFile = $PSCommandPath
+. (($CodeFile -replace '\\tests\\', '\public\')  -replace '\.tests\.ps1$', '.ps1')
+
+#endregion
+#add-PSSnapin SwisSnapin 
+
+Import-Module -name $ModPath -Force -Verbose
+
+InModuleScope PowerOrion{
+  #describes the function Get-OrionNodeID
+  Describe -Name 'Test-OrionHardwareHealthEnabled' -Tags Readonly -Fixture {
+
+    # scenario 1: call the function without arguments
+    Context 'Running with -Swisconnection'   {
+      # test 1: it does not throw an exception:
+      It -name 'runs without errors' -test {
+      
+        { Test-OrionHardwareHealthEnabled  -SwisConnection $swis -NodeID 9999} | Should Not Throw
+      }
+      It -name 'returns an  object ' -test {
+        (Test-OrionHardwareHealthEnabled  -SwisConnection $swis -NodeID 9999).gettype() | should be System.Object[]
+      }
+      It -name 'should not show hardwareenabled for a node that does not exist' -test {
+        $results = Test-OrionHardwareHealthEnabled  -SwisConnection $swis -NodeID 9999
+        $results.hardwareenabled | should be $false
+      }
+   
+    }
+  
+  }
+}
